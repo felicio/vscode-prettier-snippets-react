ed2e45861409e76737dd148c0786bfa33e9eb2ad
Felicio Mununga <felicio@users.noreply.github.com>
2017-11-11 19:58:18 +0100

    Initialize extension

---
+++ src/extension.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const vscode = require('vscode');
+const prettier = require('prettier');
+
+exports.activate = context => {
+  let subscription = undefined;
+
+  context.subscriptions.push(subscription);
+};
