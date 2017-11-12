693325b79ab807b409439cbecc8caac5a9b2fab2
Felicio Mununga <felicio@users.noreply.github.com>
2017-11-12 15:36:35 +0100

    Parse snippets from JSON document

--- src/extension.js
+++ src/extension.js
@@ -1,10 +1,20 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const vscode = require('vscode');
 const prettier = require('prettier');
 
-exports.activate = context => {
-  let subscription = undefined;
+exports.activate = () => {
+  const snippetsPath = path.resolve(__dirname, '../snippets/snippets.json');
+  let obj = undefined;
 
-  context.subscriptions.push(subscription);
+  try {
+    const snippets = fs.readFileSync(snippetsPath, 'utf8');
+    obj = JSON.parse(snippets);
+  } catch (error) {
+    console.error(error);
+  }
+
+  // TODO: Format `obj.body` before inserting to a text document
 };
